5fe8b4123e5fc6a8227bc5020e42e10589f6e990
1277762085 <cwh19961102>
2018-04-18 11:56:55 +0800

    第一次上传

---
+++ new/jianshu/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nuxt/>
+  
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap.css'
+export default{
+// 首先引入bootstrap。css的文件
+
+}
+</script>
+
+
